536e2b68c18a3171db1e2a4fec9d2a2f0af5b9c7
Jason Laster <jason.laster.11@gmail.com>
2016-12-08 20:34:26 -0500

    strip amd

--- src/reps/number.js
+++ src/reps/number.js
@@ -1,52 +1,39 @@
-/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
-/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-"use strict";
+const React = require("devtools/client/shared/vendor/react");
 
-// Make this available to both AMD and CJS environments
-define(function (require, exports, module) {
-  // Dependencies
-  const React = require("devtools/client/shared/vendor/react");
+// Shortcuts
+const { span } = React.DOM;
 
-  // Shortcuts
-  const { span } = React.DOM;
+/**
+ * Renders a number
+ */
+const Number = React.createClass({
+  displayName: "Number",
 
-  /**
-   * Renders a number
-   */
-  const Number = React.createClass({
-    displayName: "Number",
+  stringify: function (object) {
+    let isNegativeZero = Object.is(object, -0) ||
+      (object.type && object.type == "-0");
 
-    stringify: function (object) {
-      let isNegativeZero = Object.is(object, -0) ||
-        (object.type && object.type == "-0");
+    return (isNegativeZero ? "-0" : String(object));
+  },
 
-      return (isNegativeZero ? "-0" : String(object));
-    },
+  render: function () {
+    let value = this.props.object;
 
-    render: function () {
-      let value = this.props.object;
-
-      return (
-        span({className: "objectBox objectBox-number"},
-          this.stringify(value)
-        )
-      );
-    }
-  });
-
-  function supportsObject(object, type) {
-    return type == "boolean" || type == "number" ||
-      (type == "object" && object.type == "-0");
+    return (
+      span({className: "objectBox objectBox-number"},
+        this.stringify(value)
+      )
+    );
   }
-
-  // Exports from this module
-
-  exports.Number = {
-    rep: Number,
-    supportsObject: supportsObject
-  };
 });
+
+function supportsObject(object, type) {
+  return type == "boolean" || type == "number" ||
+    (type == "object" && object.type == "-0");
+}
+
+module.exports = {
+  rep: Number,
+  supportsObject: supportsObject
+};
--- src/reps/rep-utils.js
+++ src/reps/rep-utils.js
@@ -1,157 +1,145 @@
-/* globals URLSearchParams */
-/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
-/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+const React = require("devtools/client/shared/vendor/react");
 
-"use strict";
+/**
+ * Create React factories for given arguments.
+ * Example:
+ *   const { Rep } = createFactories(require("./rep"));
+ */
+function createFactories(args) {
+  let result = {};
+  for (let p in args) {
+    result[p] = React.createFactory(args[p]);
+  }
+  return result;
+}
 
-// Make this available to both AMD and CJS environments
-define(function (require, exports, module) {
-  // Dependencies
-  const React = require("devtools/client/shared/vendor/react");
+/**
+ * Returns true if the given object is a grip (see RDP protocol)
+ */
+function isGrip(object) {
+  return object && object.actor;
+}
 
-  /**
-   * Create React factories for given arguments.
-   * Example:
-   *   const { Rep } = createFactories(require("./rep"));
-   */
-  function createFactories(args) {
-    let result = {};
-    for (let p in args) {
-      result[p] = React.createFactory(args[p]);
-    }
-    return result;
+function escapeNewLines(value) {
+  return value.replace(/\r/gm, "\\r").replace(/\n/gm, "\\n");
+}
+
+function cropMultipleLines(text, limit) {
+  return escapeNewLines(cropString(text, limit));
+}
+
+function cropString(text, limit, alternativeText) {
+  if (!alternativeText) {
+    alternativeText = "\u2026";
   }
 
-  /**
-   * Returns true if the given object is a grip (see RDP protocol)
-   */
-  function isGrip(object) {
-    return object && object.actor;
-  }
+  // Make sure it's a string.
+  text = text + "";
 
-  function escapeNewLines(value) {
-    return value.replace(/\r/gm, "\\r").replace(/\n/gm, "\\n");
-  }
+  // Replace all non-printable characters, except of
+  // (horizontal) tab (HT: \x09) and newline (LF: \x0A, CR: \x0D),
+  // with unicode replacement character (u+fffd).
+  // eslint-disable-next-line no-control-regex
+  let re = new RegExp("[\x00-\x08\x0B\x0C\x0E-\x1F\x80-\x9F]", "g");
+  text = text.replace(re, "\ufffd");
 
-  function cropMultipleLines(text, limit) {
-    return escapeNewLines(cropString(text, limit));
-  }
-
-  function cropString(text, limit, alternativeText) {
-    if (!alternativeText) {
-      alternativeText = "\u2026";
-    }
-
-    // Make sure it's a string.
-    text = text + "";
-
-    // Replace all non-printable characters, except of
-    // (horizontal) tab (HT: \x09) and newline (LF: \x0A, CR: \x0D),
-    // with unicode replacement character (u+fffd).
-    // eslint-disable-next-line no-control-regex
-    let re = new RegExp("[\x00-\x08\x0B\x0C\x0E-\x1F\x80-\x9F]", "g");
-    text = text.replace(re, "\ufffd");
-
-    // Crop the string only if a limit is actually specified.
-    if (!limit || limit <= 0) {
-      return text;
-    }
-
-    // Set the limit at least to the length of the alternative text
-    // plus one character of the original text.
-    if (limit <= alternativeText.length) {
-      limit = alternativeText.length + 1;
-    }
-
-    let halfLimit = (limit - alternativeText.length) / 2;
-
-    if (text.length > limit) {
-      return text.substr(0, Math.ceil(halfLimit)) + alternativeText +
-        text.substr(text.length - Math.floor(halfLimit));
-    }
-
+  // Crop the string only if a limit is actually specified.
+  if (!limit || limit <= 0) {
     return text;
   }
 
-  function parseURLParams(url) {
-    url = new URL(url);
-    return parseURLEncodedText(url.searchParams);
+  // Set the limit at least to the length of the alternative text
+  // plus one character of the original text.
+  if (limit <= alternativeText.length) {
+    limit = alternativeText.length + 1;
   }
 
-  function parseURLEncodedText(text) {
-    let params = [];
+  let halfLimit = (limit - alternativeText.length) / 2;
 
-    // In case the text is empty just return the empty parameters
-    if (text == "") {
-      return params;
-    }
-
-    let searchParams = new URLSearchParams(text);
-    let entries = [...searchParams.entries()];
-    return entries.map(entry => {
-      return {
-        name: entry[0],
-        value: entry[1]
-      };
-    });
+  if (text.length > limit) {
+    return text.substr(0, Math.ceil(halfLimit)) + alternativeText +
+      text.substr(text.length - Math.floor(halfLimit));
   }
 
-  function getFileName(url) {
-    let split = splitURLBase(url);
-    return split.name;
+  return text;
+}
+
+function parseURLParams(url) {
+  url = new URL(url);
+  return parseURLEncodedText(url.searchParams);
+}
+
+function parseURLEncodedText(text) {
+  let params = [];
+
+  // In case the text is empty just return the empty parameters
+  if (text == "") {
+    return params;
   }
 
-  function splitURLBase(url) {
-    if (!isDataURL(url)) {
-      return splitURLTrue(url);
-    }
-    return {};
+  let searchParams = new URLSearchParams(text);
+  let entries = [...searchParams.entries()];
+  return entries.map(entry => {
+    return {
+      name: entry[0],
+      value: entry[1]
+    };
+  });
+}
+
+function getFileName(url) {
+  let split = splitURLBase(url);
+  return split.name;
+}
+
+function splitURLBase(url) {
+  if (!isDataURL(url)) {
+    return splitURLTrue(url);
   }
+  return {};
+}
 
-  function getURLDisplayString(url) {
-    return cropString(url);
-  }
+function getURLDisplayString(url) {
+  return cropString(url);
+}
 
-  function isDataURL(url) {
-    return (url && url.substr(0, 5) == "data:");
-  }
+function isDataURL(url) {
+  return (url && url.substr(0, 5) == "data:");
+}
 
-  function splitURLTrue(url) {
-    const reSplitFile = /(.*?):\/{2,3}([^\/]*)(.*?)([^\/]*?)($|\?.*)/;
-    let m = reSplitFile.exec(url);
+function splitURLTrue(url) {
+  const reSplitFile = /(.*?):\/{2,3}([^\/]*)(.*?)([^\/]*?)($|\?.*)/;
+  let m = reSplitFile.exec(url);
 
-    if (!m) {
-      return {
-        name: url,
-        path: url
-      };
-    } else if (m[4] == "" && m[5] == "") {
-      return {
-        protocol: m[1],
-        domain: m[2],
-        path: m[3],
-        name: m[3] != "/" ? m[3] : m[2]
-      };
-    }
-
+  if (!m) {
+    return {
+      name: url,
+      path: url
+    };
+  } else if (m[4] == "" && m[5] == "") {
     return {
       protocol: m[1],
       domain: m[2],
-      path: m[2] + m[3],
-      name: m[4] + m[5]
+      path: m[3],
+      name: m[3] != "/" ? m[3] : m[2]
     };
   }
 
-  // Exports from this module
-  exports.createFactories = createFactories;
-  exports.isGrip = isGrip;
-  exports.cropString = cropString;
-  exports.cropMultipleLines = cropMultipleLines;
-  exports.parseURLParams = parseURLParams;
-  exports.parseURLEncodedText = parseURLEncodedText;
-  exports.getFileName = getFileName;
-  exports.getURLDisplayString = getURLDisplayString;
-});
+  return {
+    protocol: m[1],
+    domain: m[2],
+    path: m[2] + m[3],
+    name: m[4] + m[5]
+  };
+}
+
+module.exports = {
+  createFactories,
+  isGrip,
+  cropString,
+  cropMultipleLines,
+  parseURLParams,
+  parseURLEncodedText,
+  getFileName,
+  getURLDisplayString
+}
