2c5175f3a9a68132db75849ef3785cb387e400c5
Julian Descottes <julian.descottes@gmail.com>
2016-12-20 23:10:24 +0100

    support symlink development mode (#21)

--- packages/devtools-launchpad/index.js
+++ packages/devtools-launchpad/index.js
@@ -1,9 +1,9 @@
 const developmentServer = require("./bin/development-server");
 const toolboxConfig = require("./webpack.config");
-const tools = require("./src/tools");
+// const tools = require("./src/tools");
 
 module.exports = {
   startDevServer: developmentServer.startDevServer,
   toolboxConfig,
-  tools
+  // tools
 };
