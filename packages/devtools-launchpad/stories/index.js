160363747689c79a5c13528299bce94b9a7c1feb
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2016-12-27 11:53:09 +0100

    Add a story to test filtered tabs
    It also adds a way to get the tabs like we would have it from the redux store.
    Doing that allow us to be confident about how things are piped.

--- packages/devtools-launchpad/stories/index.js
+++ packages/devtools-launchpad/stories/index.js
@@ -1,7 +1,12 @@
 const React = require("react");
-const { storiesOf } = require("@kadira/storybook");
+const { storiesOf, action } = require("@kadira/storybook");
 const LandingPage = require("../src/components/LandingPage");
-const { Map } = require("immutable");
+
+const { combineReducers } = require("redux");
+let reducers = require("../src/reducers");
+let constants = require("../src/constants");
+let getState = combineReducers(reducers);
+const selectors = require("../src/selectors");
 
 // Add devtools theme styles
 require("../src/lib/themes/light-theme.css");
@@ -11,69 +16,103 @@ const getTab = (id, title, clientType, url) => {
   if (!url) {
     url = `/test/${id}`;
   }
-  return [id, Map({
+
+  return {
     url,
     clientType,
-    id,
+    id: `${id}`,
     title
-  })];
+  };
+};
+
+const getTabs = (tabs, state) => {
+  let newState = getState(state, {
+    type: constants.ADD_TABS,
+    value: tabs
+  });
+
+  return selectors.getTabs(newState);
 };
 
 const renderLandingPage = (props) => {
-  return React.DOM.div({}, React.createElement(LandingPage, props));
+  return React.DOM.div({}, React.createElement(LandingPage, Object.assign({
+    onFilterChange: action("FILTER_TABS")
+  }, props)));
 };
 
 storiesOf("LandingPage", module)
   .add("six firefox tabs", () => {
-    let tabs = Map([
+    let tabs = [
       getTab(1, "Page 1", "firefox"),
       getTab(2, "Page 2", "firefox"),
       getTab(3, "Page 3", "firefox"),
       getTab(4, "Page 4", "firefox"),
       getTab(5, "Page 5", "firefox"),
       getTab(6, "Page 6", "firefox"),
-    ]);
+    ];
 
     return renderLandingPage({
-      tabs,
+      tabs: getTabs(tabs),
       supportsFirefox: true,
       supportsChrome: true,
       title: "Storybook test"
     });
   })
   .add("two of each", () => {
-    let tabs = Map([
+    let tabs = [
       getTab(1, "Page 1", "firefox"),
       getTab(2, "Page 2", "firefox"),
       getTab(3, "Page 3", "chrome"),
       getTab(4, "Page 4", "chrome"),
       getTab(5, "process 1", "node"),
       getTab(6, "process 2", "node"),
-    ]);
+    ];
 
     return renderLandingPage({
-      tabs,
+      tabs: getTabs(tabs),
       supportsFirefox: true,
       supportsChrome: true,
       title: "Storybook test"
     });
   })
   .add("one hundred firefox tabs with long titles and URLs", () => {
-    let tabs = Map(
-      Array(100)
-        .fill()
-        .map((_, i) =>
-          getTab(
-            i + 1,
-            `My very long title ${(`${i + 1}-`).repeat(50)}`,
-            "firefox",
-            `this/is/a/very/long/url/${(`${i + 1}/`).repeat(50)}`,
-          )
+    let tabs = Array(100)
+      .fill()
+      .map((_, i) =>
+        getTab(
+          i + 1,
+          `My very long title ${(`${i + 1}-`).repeat(50)}`,
+          "firefox",
+          `this/is/a/very/long/url/${(`${i + 1}/`).repeat(50)}`,
         )
-    );
+      );
 
     return renderLandingPage({
-      tabs,
+      tabs: getTabs(tabs),
+      supportsFirefox: true,
+      supportsChrome: true,
+      title: "Storybook test"
+    });
+  })
+  .add("six firefox tabs, filtered with \"MoZ\" (two tabs match)", () => {
+    let filterString = "MoZ";
+    let tabs = [
+      getTab(1, "Mozilla", "firefox"),
+      getTab(2, "Page 2", "firefox", "https://mozilla.com"),
+      getTab(3, "Page 3", "firefox"),
+      getTab(4, "Page 4", "firefox"),
+      getTab(5, "Page 5", "firefox"),
+      getTab(6, "Page 6", "firefox"),
+    ];
+
+    let state = getState(undefined, {
+      type: constants.FILTER_TABS,
+      value: filterString
+    });
+
+    return renderLandingPage({
+      tabs: getTabs(tabs, state),
+      filterString: selectors.getFilterString(state),
       supportsFirefox: true,
       supportsChrome: true,
       title: "Storybook test"
