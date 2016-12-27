88aea312a53e5d49bbc21ecc76387fa7c810f4e4
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2016-12-27 16:30:07 +0100

    Refactor quick links
    Create a dedicated components.
    Add more links.
    Make "yolo" link add all the expressions from the quick links.
    Add some styling.

--- src/launchpad/components/QuickLinks.js
+++ src/launchpad/components/QuickLinks.js
@@ -1,8 +1,6 @@
 const React = require("react");
-const { createFactory, DOM: dom, PropTypes } = React;
-
-const QuickLinks = createFactory(require("./QuickLinks"));
-require("./Header.css");
+const { DOM: dom, PropTypes } = React;
+require("./QuickLinks.css");
 
 const expressions = {
   array: [
@@ -84,17 +82,16 @@ expressions.yolo = Object.keys(expressions).reduce((res, key) => {
   return [...res, ...expressions[key]];
 }, []);
 
-const Header = React.createClass({
+const QuickLinks = React.createClass({
 
   propTypes: {
-    evaluate: PropTypes.func.required,
-    clearResultsList: PropTypes.func.required,
+    evaluate: PropTypes.function
   },
 
-  displayName: "Header",
+  displayName: "QuickLinks",
 
-  evaluateExpressions(label) {
-    expressions[label].forEach(
+  evaluateExpressions(sampleExpressions) {
+    sampleExpressions.forEach(
       expression => this.props.evaluate(expression)
     );
   },
@@ -103,44 +100,19 @@ const Header = React.createClass({
     return Object.keys(expressions).map(label => dom.span(
       {
         key: label,
-        onClick: () => this.evaluateExpressions(label)
+        "data-expressions-count": expressions[label].length,
+        onClick: () => this.evaluateExpressions(expressions[label])
       },
       label
     ));
   },
 
-  onSubmitForm: function(e) {
-    e.preventDefault();
-    let data = new FormData(e.target);
-    let expression = data.get("expression");
-    this.props.evaluate(expression);
-  },
-
-  onClearButtonClick: function(e) {
-    this.props.clearResultsList();
-  },
-
   render() {
-    return dom.header(
-      { className: "console-header" },
-      dom.form({
-          onSubmit: this.onSubmitForm,
-        },
-        dom.h1({}, "Reps"),
-        dom.input({
-          type: "text",
-          placeholder: "Enter an expression",
-          name: "expression"
-        }),
-        dom.button({
-          className: "clear-button",
-          type: "button",
-          onClick: this.onClearButtonClick
-        }, "Clear"),
-      ),
-      QuickLinks({evaluate: this.props.evaluate})
+    return dom.div(
+      { className: "quick-links" },
+      this.renderLinks()
     );
   }
 });
 
-module.exports = Header;
+module.exports = QuickLinks;
