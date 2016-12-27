27c317a84b9dbdd7299c21927b1a3c94bdb8900b
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2016-12-27 22:23:09 +0100

    Refactor QuickLinks
    Add a Sample.js file which contains the expressions by type (not using
    a JSON file because it s nice to have template literals for multiline
    expressions).
    Removed useless expressions from Header.
    Modify the links to render as button and add a title on them

--- src/launchpad/components/Header.js
+++ src/launchpad/components/Header.js
@@ -4,86 +4,6 @@ const { createFactory, DOM: dom, PropTypes } = React;
 const QuickLinks = createFactory(require("./QuickLinks"));
 require("./Header.css");
 
-const expressions = {
-  array: [
-    "x = [1, \"2\", {three: 3}, []]",
-    "x = []"
-  ],
-
-  boolean: [
-    "true",
-    "false"
-  ],
-
-  date: [
-    "new Date()"
-  ],
-
-  function: [
-    "x = () => { 2 }"
-  ],
-
-  node: [
-    `x = document.createElement("div");
-     x.setAttribute("id", "myNodeId");
-     x.setAttribute("class", "my-class and another");
-     x.textContent = "My node id";
-     x;
-    `,
-    `x = document.createElementNS("http://www.w3.org/2000/svg", "clipPath");
-     x.setAttribute("id", "myNodeId");
-     x.setAttribute("class", "my-class and another");
-     x;
-    `,
-    "document.createComment('my comment node')",
-    "document.createTextNode('foo')",
-    `x = document.createAttribute('foo');
-     x.value = "bar";
-     x;
-    `
-  ],
-
-  number: [
-    "1",
-    "-1",
-    "-3.14",
-    "0",
-    "-0",
-    "Infinity",
-    "-Infinity",
-    "NaN"
-  ],
-
-  object: [
-    "x = {a: 2}"
-  ],
-
-  promise: [
-    "Promise.resolve([1, 2, 3])",
-    "Promise.reject(new Error('This is wrong'))",
-    "new Promise()"
-  ],
-
-  regexp: [
-    "new RegExp('^[-]?[0-9]+[\.]?[0-9]+$')"
-  ],
-
-  string: [
-    "'foo'",
-    "'bar\nbaz\nyup'",
-    "'blah'.repeat(10000)"
-  ],
-
-  symbol: [
-    "Symbol('foo')",
-    "Symbol()"
-  ]
-};
-
-expressions.yolo = Object.keys(expressions).reduce((res, key) => {
-  return [...res, ...expressions[key]];
-}, []);
-
 const Header = React.createClass({
 
   propTypes: {
@@ -92,22 +12,6 @@ const Header = React.createClass({
   },
 
   displayName: "Header",
-
-  evaluateExpressions(label) {
-    expressions[label].forEach(
-      expression => this.props.evaluate(expression)
-    );
-  },
-
-  renderLinks() {
-    return Object.keys(expressions).map(label => dom.span(
-      {
-        key: label,
-        onClick: () => this.evaluateExpressions(label)
-      },
-      label
-    ));
-  },
 
   onSubmitForm: function(e) {
     e.preventDefault();
--- src/launchpad/components/QuickLinks.js
+++ src/launchpad/components/QuickLinks.js
@@ -1,110 +1,38 @@
 const React = require("react");
 const { DOM: dom, PropTypes } = React;
 require("./QuickLinks.css");
-
-const expressions = {
-  array: [
-    "x = [1, \"2\", {three: 3}, []]",
-    "x = []"
-  ],
-
-  boolean: [
-    "true",
-    "false"
-  ],
-
-  date: [
-    "new Date()"
-  ],
-
-  function: [
-    "x = () => { 2 }"
-  ],
-
-  node: [
-    `x = document.createElement("div");
-     x.setAttribute("id", "myNodeId");
-     x.setAttribute("class", "my-class and another");
-     x.textContent = "My node id";
-     x;
-    `,
-    `x = document.createElementNS("http://www.w3.org/2000/svg", "clipPath");
-     x.setAttribute("id", "myNodeId");
-     x.setAttribute("class", "my-class and another");
-     x;
-    `,
-    "document.createComment('my comment node')",
-    "document.createTextNode('foo')",
-    `x = document.createAttribute('foo');
-     x.value = "bar";
-     x;
-    `
-  ],
-
-  number: [
-    "1",
-    "-1",
-    "-3.14",
-    "0",
-    "-0",
-    "Infinity",
-    "-Infinity",
-    "NaN"
-  ],
-
-  object: [
-    "x = {a: 2}"
-  ],
-
-  promise: [
-    "Promise.resolve([1, 2, 3])",
-    "Promise.reject(new Error('This is wrong'))",
-    "new Promise()"
-  ],
-
-  regexp: [
-    "new RegExp('^[-]?[0-9]+[\.]?[0-9]+$')"
-  ],
-
-  string: [
-    "'foo'",
-    "'bar\nbaz\nyup'",
-    "'blah'.repeat(10000)"
-  ],
-
-  symbol: [
-    "Symbol('foo')",
-    "Symbol()"
-  ]
-};
-
-expressions.yolo = Object.keys(expressions).reduce((res, key) => {
-  return [...res, ...expressions[key]];
-}, []);
+const samples = require("./Samples.js");
 
 const QuickLinks = React.createClass({
 
   propTypes: {
-    evaluate: PropTypes.function
+    evaluate: PropTypes.func.required
   },
 
   displayName: "QuickLinks",
 
-  evaluateExpressions(sampleExpressions) {
-    sampleExpressions.forEach(
+  evaluateExpressions(expressions) {
+    expressions.forEach(
       expression => this.props.evaluate(expression)
     );
   },
 
   renderLinks() {
-    return Object.keys(expressions).map(label => dom.span(
-      {
-        key: label,
-        "data-expressions-count": expressions[label].length,
-        onClick: () => this.evaluateExpressions(expressions[label])
-      },
-      label
-    ));
+    return Object.keys(samples)
+      .map(label => {
+        let expressions = samples[label];
+        let length =  expressions.length;
+        return dom.button(
+          {
+            key: label,
+            title: label === "yolo"
+              ? "Add all sample expressions"
+              : `Add ${length} ${label} sample expression${length > 1 ? "s" : ""}`,
+            onClick: () => this.evaluateExpressions(expressions)
+          },
+          label
+        )
+      });
   },
 
   render() {
