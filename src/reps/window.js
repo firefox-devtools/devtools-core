27096750432eccf8fc2565b943d61c8e8c9b4d61
Jason Laster <jason.laster.11@gmail.com>
2017-01-10 16:45:38 -0500

    Fix window rep

--- src/reps/window.js
+++ src/reps/window.js
@@ -1,4 +1,5 @@
 const React = require("react");
+const { MODE } = require("./constants");
 
 // Reps
 const { isGrip, getURLDisplayString } = require("./rep-utils");
@@ -14,6 +15,7 @@ let Window = React.createClass({
 
   propTypes: {
     object: React.PropTypes.object.isRequired,
+    mode: React.PropTypes.oneOf(Object.keys(MODE).map(key => MODE[key])),
   },
 
   getTitle: function (grip) {
@@ -31,6 +33,14 @@ let Window = React.createClass({
     return getURLDisplayString(grip.preview.url);
   },
 
+  getDisplayValue: function (grip) {
+    if (this.props.mode === MODE.TINY) {
+      return grip.isGlobal ? "Global" : "Window";
+    }
+
+    return this.getLocation(grip);
+  },
+
   render: function () {
     let grip = this.props.object;
 
@@ -38,7 +48,7 @@ let Window = React.createClass({
       DOM.span({className: "objectBox objectBox-Window"},
         this.getTitle(grip),
         DOM.span({className: "objectPropValue"},
-          this.getLocation(grip)
+          this.getDisplayValue(grip)
         )
       )
     );
