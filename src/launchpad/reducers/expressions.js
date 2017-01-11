1ca947243cd95b173b7c0234bfbb40afd85c21cd
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2017-01-11 21:17:20 +0100

    Store expressions in localStorage (#37)

--- src/launchpad/reducers/expressions.js
+++ src/launchpad/reducers/expressions.js
@@ -1,16 +1,31 @@
 const constants = require("../constants");
 const Immutable = require("immutable");
 
-const initialState = Immutable.Map();
+let storedExpressions;
+try {
+  storedExpressions = JSON.parse(
+    window.localStorage.getItem(constants.LS_EXPRESSIONS_KEY)
+  );
+} catch (e) {}
+
+const initialState = storedExpressions
+  ? Immutable.fromJS(storedExpressions)
+  : Immutable.Map();
 
 function update(state = initialState, action) {
   const { type, value, key } = action;
 
   switch (type) {
     case constants.ADD_EXPRESSION:
-      return state.set(key, Immutable.Map(value));
+      let newState = state.set(key, Immutable.Map(value));
+      window.localStorage.setItem(
+        constants.LS_EXPRESSIONS_KEY, 
+        JSON.stringify(newState.toJS())
+      );
+      return newState;
 
     case constants.CLEAR_EXPRESSIONS:
+      window.localStorage.removeItem(constants.LS_EXPRESSIONS_KEY);
       return state.clear();
 
     case constants.SHOW_RESULT_PACKET:
