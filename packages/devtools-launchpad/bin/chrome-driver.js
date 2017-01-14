618dce9de54a848ca2f40f0722122b26a90fd4b8
bomsy <b4bomsy@gmail.com>
2017-01-14 17:31:12 +0000

    Enable loading chrome with url to go to

--- packages/devtools-launchpad/bin/chrome-driver.js
+++ packages/devtools-launchpad/bin/chrome-driver.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
 const spawn = require('child_process').spawn;
+const minimist = require("minimist");
 
 const chromeBinary = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+const args = minimist(process.argv.slice(2), {
+  string: ["location"]
+});
 
-const chrome = spawn(chromeBinary, [
+let chromeArgs = [
   "--remote-debugging-port=9222",
   "--no-first-run",
   "--user-data-dir=/tmp/chrome-dev-profile"
-])
+];
+
+chromeArgs.push(args.location ? args.location : "about:blank");
+
+const chrome = spawn(chromeBinary, chromeArgs);
 
 chrome.stdout.on('data', data => console.log(`stdout: ${data}`));
 chrome.stderr.on('data', data => console.log(`stderr: ${data}`));
