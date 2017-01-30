edfe3304362db066f7d66ed1d3e54bd6be5c1703
Jason Laster <jason.laster.11@gmail.com>
2017-01-30 09:43:28 -0500

    Revert "Support loading chrome scopes and variables (#131)"
    This reverts commit 42d8e31f05f28e53a3136b198c373d14a125be68.

--- packages/devtools-client-adapters/src/chrome/commands.js
+++ packages/devtools-client-adapters/src/chrome/commands.js
@@ -1,8 +1,6 @@
 // @flow
 
-const {
-  toServerLocation, fromServerLocation, createLoadedObject
-} = require("./create");
+const { toServerLocation, fromServerLocation } = require("./create");
 
 import type { Location } from "../types";
 import type { ServerLocation, Agents } from "./types";
@@ -76,16 +74,6 @@ function removeBreakpoint(breakpointId: string) {
   return debuggerAgent.removeBreakpoint({ breakpointId });
 }
 
-async function getProperties(object: any) {
-  const { result } = await runtimeAgent.getProperties({
-    objectId: object.objectId
-  });
-
-  const loadedObjects = result.map(createLoadedObject);
-
-  return { loadedObjects };
-}
-
 function evaluate(script: string) {
   return runtimeAgent.evaluate({ expression: script });
 }
@@ -111,8 +99,7 @@ const clientCommands = {
   removeBreakpoint,
   evaluate,
   debuggeeCommand,
-  navigate,
-  getProperties
+  navigate
 };
 
 module.exports = {
