2895a4d38e8f3db10eb8a521400190f302e73fff
Julian Descottes <julian.descottes@gmail.com>
2017-01-30 01:11:36 +0100

    fix eslint errors

--- src/reps/load-reps.js
+++ src/reps/load-reps.js
@@ -3,8 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-"use strict";
-
 // Make this available to both AMD and CJS environments
 define(function (require, exports, module) {
   let REPS;
