17eb044fb52d067096743da644b148de11ef1c30
Jan Odvarko <odvarko@gmail.com>
2017-01-31 19:53:41 +0100

    Update assets tools

--- packages/devtools-jsonview/bin/publish-assets.js
+++ packages/devtools-jsonview/bin/publish-assets.js
@@ -1,16 +1,13 @@
-const { tools: { makeBundle, symlinkTests, copyFile }} = require("devtools-launchpad/index");
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { tools: { makeBundle }} = require("devtools-launchpad/index");
 const path = require("path");
 
 function start() {
   console.log("start: publish assets");
   const projectPath = path.resolve(__dirname, "..");
-  const mcModulePath = "devtools/client/shared/components/reps";
-
-  copyFile(
-    path.resolve(projectPath, "src/test/mochitest"),
-    path.resolve(projectPath, "assets/build"),
-    { cwd: projectPath }
-  );
 
   makeBundle({
     outputPath: `${projectPath}/assets/build`,
