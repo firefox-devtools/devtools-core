ff0c42ad3a3b999afcdd0f808008cda664aec5f1
Jan Odvarko <odvarko@gmail.com>
2017-01-31 09:36:27 +0100

    Introduce Tabs and TreeView components

--- packages/devtools-modules/index.js
+++ packages/devtools-modules/index.js
@@ -1,11 +1,16 @@
 const Services = require("./client/shared/shim/Services");
 const SplitBox = require("./client/shared/components/splitter/SplitBox");
 // const SplitBoxCSS = require("./client/shared/components/splitter/SplitBox.css")
+const { Tabs, TabPanel } = require("./client/shared/components/tabs/Tabs");
+const TreeView = require("./client/shared/components/tree/tree-view");
 const sprintf = require("./shared/sprintf").sprintf;
 
 module.exports = {
   Services,
   SplitBox,
   // SplitBoxCSS,
+  Tabs,
+  TabPanel,
+  TreeView,
   sprintf
 };
