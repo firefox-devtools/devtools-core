f825d0012231bc171a237922be74e8949556dfec
Jan Odvarko <odvarko@gmail.com>
2017-01-31 19:59:42 +0100

    Customize dev server

--- packages/devtools-jsonview/bin/dev-server.js
+++ packages/devtools-jsonview/bin/dev-server.js
@@ -5,6 +5,9 @@
 const toolbox = require("devtools-launchpad/index");
 const feature = require("devtools-config");
 const getConfig = require("./getConfig");
+const getValue = require("devtools-config").getValue;
+const Mustache = require("mustache");
+const isDevelopment = require("devtools-config").isDevelopment;
 
 const fs = require("fs");
 const path = require("path");
@@ -16,22 +19,51 @@ const webpackConfig = require("../webpack.config");
 let { app } = toolbox.startDevServer(envConfig, webpackConfig);
 
 /**
+ * Serve the root file for launchpad.
+ */
+app.get("/", (req, res) => {
+  const tplPath = path.join(__dirname, "../index.html");
+  const tplFile = fs.readFileSync(tplPath, "utf8");
+  const bundleName = "bundle";
+
+  res.send(Mustache.render(tplFile, {
+    isDevelopment: isDevelopment(),
+    dir: getValue("dir") || "ltr",
+    bundleName,
+    title: getValue("title"),
+    favicon: getFavicon()
+  }));
+});
+
+/**
  * Provide images used in CSS.
  */
 function sendFile(res, src, encoding) {
   const filePath = path.join(__dirname, src);
-  const file = encoding ? fs.readFileSync(filePath, encoding) : fs.readFileSync(filePath);
+  const file = encoding ? fs.readFileSync(filePath, encoding) :
+    fs.readFileSync(filePath);
   res.send(file);
 }
 
-app.get("/images/:file.svg", function (req, res) {
-  console.log("Get SVG file: " + req.params.file);
+app.get("/images/:file.svg", function(req, res) {
+  console.log(`Get SVG file: ${req.params.file}`);
   res.contentType("image/svg+xml");
-  sendFile(res, "../assets/images/" + req.params.file + ".svg", "utf-8");
+  sendFile(res, `../assets/images/${req.params.file}.svg`, "utf-8");
 });
 
-app.get("/images/:file.png", function (req, res) {
-  console.log("Get PNG file: " + req.params.file);
+app.get("/images/:file.png", function(req, res) {
+  console.log(`Get PNG file: ${req.params.file}`);
   res.contentType("image/png");
-  sendFile(res, "../assets/images/" + req.params.file + ".png");
+  sendFile(res, `../assets/images/${req.params.file}.png`);
 });
+
+/**
+ * Helpers
+ */
+function getFavicon() {
+  let favicon = getValue("favicon");
+  if (!favicon) {
+    return "/images/favicon.png";
+  }
+  return path.basename(favicon);
+}
