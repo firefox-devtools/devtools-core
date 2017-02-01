499963d704bb14e467d624d17ca7396ab49225be
Jason Laster <jason.laster.11@gmail.com>
2017-02-01 09:00:36 -0500

    update stubNetworkRequest

--- packages/devtools-network-request/stubNetworkRequest.js
+++ packages/devtools-network-request/stubNetworkRequest.js
@@ -1,9 +1,14 @@
-const fs = require("fs");
 module.exports = function(url) {
+  const fs = require("fs");
+  const _path = require("path");
+
   return new Promise((resolve, reject) => {
     // example.com is used at a dummy URL that points to our local
     // `/src` folder.
-    url = url.replace("http://example.com/test/", "/../../src/test/mochitest/examples/");
-    resolve({ content: fs.readFileSync(__dirname + url, "utf8") });
+    url = url.replace(/http:\/\/localhost:8000/, "");
+    const requestUrl = _path.join(__dirname, "/../../src/test/mochitest/", url);
+    const content = fs.readFileSync(requestUrl, "utf8");
+
+    resolve({ content });
   });
 };
