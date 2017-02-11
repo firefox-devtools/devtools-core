13f312f781d4eecc15209b2e4a9cabb8496e2a1e
Julian Descottes <julian.descottes@gmail.com>
2017-02-11 12:45:13 +0100

    Simplify workflow for deploying in mc

--- bin/copy-assets.js
+++ bin/copy-assets.js
@@ -1,37 +1,69 @@
 const { tools: { makeBundle, symlinkTests, copyFile }} = require("devtools-launchpad/index");
+const fs = require('fs-extra')
 const path = require("path");
 const minimist = require("minimist");
+const getConfig = require("./getConfig");
+const { getValue, setConfig } = require("devtools-config");
 
 const args = minimist(process.argv.slice(2), {
-  boolean: ["watch"],
+  boolean: ["watch", "symlink"],
 });
 
 function start() {
   console.log("start: copy assets");
   const projectPath = path.resolve(__dirname, "..");
   const mcModulePath = "devtools/client/shared/components/reps";
-  const mcPath = "./firefox";
 
-  symlinkTests({ projectPath, mcModulePath });
+  const envConfig = getConfig();
+  setConfig(envConfig);
 
+  const mcPath = getValue("firefox.mcPath");
+  const shouldSymlink = args.symlink;
+
+  if (mcPath !== "./firefox" && shouldSymlink) {
+    console.error("Aborting copy-assets...");
+    console.error("[--symlink] option only available if mcPath is `./firefox`");
+    return;
+  }
+
+  console.log("  output path is:", mcPath);
+  console.log("  copying load-reps...");
   copyFile(
     path.resolve(projectPath, "src/reps/load-reps.js"),
     path.join(mcPath, mcModulePath, "load-reps.js"),
     {cwd: projectPath}
   );
 
+  if (shouldSymlink) {
+    console.log("  copying tests...");
+    symlinkTests({ projectPath, mcModulePath });
+  } else {
+    console.log("  copying tests...");
+    copyFile(
+      path.resolve(projectPath, "src/test/mochitest/"),
+      path.join(mcPath, mcModulePath, "test/mochitest/"),
+      {cwd: projectPath}
+    );
+  }
+
+  console.log("  copying reps.css...");
   copyFile(
     path.resolve(projectPath, "src/reps/reps.css"),
     path.join(mcPath, mcModulePath, "reps.css"),
     {cwd: projectPath}
   );
 
+  console.log("  creating reps.js bundle...");
   makeBundle({
-    outputPath: path.join(projectPath, mcPath, mcModulePath),
+    outputPath: path.join(mcPath, mcModulePath),
     projectPath,
     watch: args.watch
   }).then(() => {
+    console.log("  remove build artifacts...");
+    fs.removeSync(path.join(mcPath, mcModulePath, "reps.js.map"));
     console.log("done: copy assets");
+  }).catch(e => {
+    console.error(e);
   });
 }
 
