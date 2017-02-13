a9da4f875c47d25b0d94f1b6cd051154f1cc1e2d
Julian Descottes <julian.descottes@gmail.com>
2017-02-13 12:05:35 +0100

    symlink test should support custom test and mc-clone paths

--- packages/devtools-launchpad/src/tools/mc/symlink-tests.js
+++ packages/devtools-launchpad/src/tools/mc/symlink-tests.js
@@ -12,17 +12,34 @@ function rm(file, { cwd }) {
   });
 }
 
-function symlinkTests({ projectPath, mcModulePath }) {
-  const projectMochitestPath =
-    `${projectPath}/src/test/mochitest`;
-  const mcMochitestPath =
-    `${projectPath}/firefox/${mcModulePath}/test/mochitest`;
+/**
+ * Symlink tests between a project using devtools-launchpad and a working
+ * mozilla-central clone.
+ *
+ * @param {String} projectPath
+ *        Path to the current project working dir.
+ * @param {String} projectTestPath
+ *        Path to the source test folder (should be located in project).
+ * @param {String} mcTestPath
+ *        Path to the target test folder (should be located in mozilla-central
+ *        clone).
+ * @param {String} (deprecated) mcModulePath
+ */
+function symlinkTests({ projectPath, projectTestPath, mcTestPath,
+  mcModulePath }) {
+  // Backwards compatibility.
+  if (mcModulePath && !projectTestPath && !mcTestPath) {
+    console.error("mcModulePath is deprecated, please define projectTestPath" +
+      " and mcTestPath");
+    projectTestPath = `${projectPath}/src/test/mochitest`;
+    mcTestPath = `${projectPath}/firefox/${mcModulePath}/test/mochitest`;
+  }
 
-  rm(mcMochitestPath, { cwd: projectPath });
+  rm(mcTestPath, { cwd: projectPath });
 
   symlink(
-    projectMochitestPath,
-    mcMochitestPath,
+    projectTestPath,
+    mcTestPath,
     { cwd: projectPath }
   );
 }
