410341fd48526ebe8ee47b6a955746b7ca320ed2
Hubert Boma Manilla <b4bomsy@gmail.com>
2017-02-13 19:39:50 +0000

    Hot reload (#187)
    * add hot reloading
    
    * expose hot reloading config to the settings pane
    
    * fix lint issues

--- packages/devtools-launchpad/index.js
+++ packages/devtools-launchpad/index.js
@@ -1,7 +1,7 @@
 const developmentServer = require("./bin/development-server");
 const toolboxConfig = require("./webpack.config");
 const tools = require("./src/tools");
-const developmentConfig = require("./config/development.json");
+const developmentConfig = require("./configs/development.json");
 
 module.exports = {
   startDevServer: developmentServer.startDevServer,
