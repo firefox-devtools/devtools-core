af95db2b5ae1a9b1e1115cd80cdc6491dfc520b3
J. Ryan Stinnett <jryans@gmail.com>
2017-02-17 18:04:23 -0600

    Add dependencies and extra modules

--- packages/devtools-source-map/src/util.js
+++ packages/devtools-source-map/src/util.js
@@ -19,6 +19,91 @@ function isGeneratedId(id: string) {
   return !isOriginalId(id);
 }
 
+/**
+ * Trims the query part or reference identifier of a URL string, if necessary.
+ */
+function trimUrlQuery(url: string): string {
+  let length = url.length;
+  let q1 = url.indexOf("?");
+  let q2 = url.indexOf("&");
+  let q3 = url.indexOf("#");
+  let q = Math.min(q1 != -1 ? q1 : length,
+                   q2 != -1 ? q2 : length,
+                   q3 != -1 ? q3 : length);
+
+  return url.slice(0, q);
+}
+
+/**
+ * Returns true if the specified URL and/or content type are specific to
+ * JavaScript files.
+ *
+ * @return boolean
+ *         True if the source is likely JavaScript.
+ */
+function isJavaScript(url: string, contentType: string = ""): boolean {
+  return (url && /\.(jsm|js)?$/.test(trimUrlQuery(url))) ||
+         contentType.includes("javascript");
+}
+
+function getContentType(url: string) {
+  if (isJavaScript(url)) {
+    return "text/javascript";
+  }
+
+  if (url.match(/ts$/)) {
+    return "text/typescript";
+  }
+
+  if (url.match(/tsx$/)) {
+    return "text/typescript-jsx";
+  }
+
+  if (url.match(/jsx$/)) {
+    return "text/jsx";
+  }
+
+  if (url.match(/coffee$/)) {
+    return "text/coffeescript";
+  }
+
+  if (url.match(/elm$/)) {
+    return "text/elm";
+  }
+
+  return "text/plain";
+}
+
+let msgId = 1;
+function workerTask(worker: any, method: string) {
+  return function(...args: any) {
+    return new Promise((resolve, reject) => {
+      const id = msgId++;
+      worker.postMessage({ id, method, args });
+
+      const listener = ({ data: result }) => {
+        if (result.id !== id) {
+          return;
+        }
+
+        worker.removeEventListener("message", listener);
+        if (result.error) {
+          reject(result.error);
+        } else {
+          resolve(result.response);
+        }
+      };
+
+      worker.addEventListener("message", listener);
+    });
+  };
+}
+
 module.exports = {
-  originalToGeneratedId, generatedToOriginalId, isOriginalId, isGeneratedId
+  originalToGeneratedId,
+  generatedToOriginalId,
+  isOriginalId,
+  isGeneratedId,
+  getContentType,
+  workerTask,
 };
