8dbf654538ca2e129879f8b1bb33cceb679a3e23
J. Ryan Stinnett <jryans@gmail.com>
2017-02-28 17:08:19 -0600

    Fix up type import paths

--- packages/devtools-source-map/src/worker.js
+++ packages/devtools-source-map/src/worker.js
@@ -18,7 +18,7 @@ const {
   getContentType,
 } = require("./util");
 
-import type { Location, Source } from "../types";
+import type { Location, Source } from "devtools-client-adapters/src/types";
 type Message = {
   data: {
     id: string,
