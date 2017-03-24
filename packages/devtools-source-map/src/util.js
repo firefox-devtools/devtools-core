b7966af550b9eefaf74fc52fe5a34aea178b9734
Jason Laster <jason.laster.11@gmail.com>
2017-03-24 16:52:35 +0100

    Start using worker-utils (#267)

--- packages/devtools-source-map/src/util.js
+++ packages/devtools-source-map/src/util.js
@@ -78,36 +78,10 @@ function getContentType(url: string) {
   return "text/plain";
 }
 
-let msgId = 1;
-function workerTask(worker: any, method: string) {
-  return function(...args: any) {
-    return new Promise((resolve, reject) => {
-      const id = msgId++;
-      worker.postMessage({ id, method, args });
-
-      const listener = ({ data: result }) => {
-        if (result.id !== id) {
-          return;
-        }
-
-        worker.removeEventListener("message", listener);
-        if (result.error) {
-          reject(result.error);
-        } else {
-          resolve(result.response);
-        }
-      };
-
-      worker.addEventListener("message", listener);
-    });
-  };
-}
-
 module.exports = {
   originalToGeneratedId,
   generatedToOriginalId,
   isOriginalId,
   isGeneratedId,
   getContentType,
-  workerTask,
 };
