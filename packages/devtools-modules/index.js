dda488d5237ac6a752ebccbf4ca18c26640ccfa9
Jason Laster <jason.laster.11@gmail.com>
2017-03-24 02:13:59 +0100

    Add worker-utils (#266)

--- packages/devtools-modules/index.js
+++ packages/devtools-modules/index.js
@@ -2,10 +2,12 @@ const Services = require("./client/shared/shim/Services");
 const SplitBox = require("./client/shared/components/splitter/SplitBox");
 // const SplitBoxCSS = require("./client/shared/components/splitter/SplitBox.css")
 const sprintf = require("./shared/sprintf").sprintf;
+const workerUtils = require("./shared/worker-utils");
 
 module.exports = {
   Services,
   SplitBox,
   // SplitBoxCSS,
-  sprintf
+  sprintf,
+  workerUtils,
 };
---
+++ packages/devtools-modules/shared/worker-utils.js
@@ -0,0 +1,28 @@
+let msgId = 1;
+function workerTask(worker: any, method: string) {
+  return function(...args: any) {
+    return new Promise((resolve, reject) => {
+      const id = msgId++;
+      worker.postMessage({ id, method, args });
+
+      const listener = ({ data: result }) => {
+        if (result.id !== id) {
+          return;
+        }
+
+        worker.removeEventListener("message", listener);
+        if (result.error) {
+          reject(result.error);
+        } else {
+          resolve(result.response);
+        }
+      };
+
+      worker.addEventListener("message", listener);
+    });
+  };
+}
+
+module.exports = {
+  workerTask,
+};
