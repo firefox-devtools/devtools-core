d1047c3a84eb444e8684db24b73d91017aee283b
Jason Laster <jason.laster.11@gmail.com>
2017-03-24 16:52:35 +0100

    Start using worker-utils (#267)

--- packages/devtools-source-map/src/index.js
+++ packages/devtools-source-map/src/index.js
@@ -1,13 +1,14 @@
 // @flow
 
-const { getValue } = require("devtools-config");
 const {
   originalToGeneratedId,
   generatedToOriginalId,
   isGeneratedId,
   isOriginalId,
-  workerTask,
 } = require("./util");
+
+const { workerUtils: { workerTask }} = require("devtools-modules");
+const { getValue } = require("devtools-config");
 
 import type { Location } from "devtools-client-adapters/src/types";
 
@@ -16,11 +17,13 @@ function restartWorker() {
   if (sourceMapWorker) {
     sourceMapWorker.terminate();
   }
-  sourceMapWorker = new Worker(getValue("sourceMapWorkerURL"));
+
+  sourceMapWorker = new Worker(getValue("workers.sourceMapURL"));
   sourceMapWorker.onerror = () => {
     console.error("Error in source map worker");
   };
 }
+
 restartWorker();
 
 function destroyWorker() {
@@ -40,12 +43,15 @@ async function hasMappedSource(location: Location): Promise<boolean> {
 }
 
 const getOriginalURLs = workerTask(sourceMapWorker, "getOriginalURLs");
-const getGeneratedLocation = workerTask(sourceMapWorker,
-                                        "getGeneratedLocation");
-const getOriginalLocation = workerTask(sourceMapWorker,
-                                       "getOriginalLocation");
-const getOriginalSourceText = workerTask(sourceMapWorker,
-                                         "getOriginalSourceText");
+const getGeneratedLocation = workerTask(
+  sourceMapWorker,
+  "getGeneratedLocation",
+);
+const getOriginalLocation = workerTask(sourceMapWorker, "getOriginalLocation");
+const getOriginalSourceText = workerTask(
+  sourceMapWorker,
+  "getOriginalSourceText",
+);
 const applySourceMap = workerTask(sourceMapWorker, "applySourceMap");
 const clearSourceMaps = workerTask(sourceMapWorker, "clearSourceMaps");
 
