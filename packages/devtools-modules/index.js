a32d5fcd03e5f23c85c4551d93cc5cbdadeba4de
Fred Lin <gasolin@mozilla.com>
2017-03-27 16:52:10 +0800

    add tree component into devtools-modules

--- packages/devtools-modules/index.js
+++ packages/devtools-modules/index.js
@@ -2,6 +2,8 @@ const Services = require("./client/shared/shim/Services");
 const SplitBox = require("./client/shared/components/splitter/SplitBox");
 // const SplitBoxCSS = require("./client/shared/components/splitter/SplitBox.css")
 const sprintf = require("./shared/sprintf").sprintf;
+const TreeRow = require("./client/shared/components/tree/tree-row");
+const TreeView = require("./client/shared/components/tree/tree-view");
 const workerUtils = require("./shared/worker-utils");
 
 module.exports = {
@@ -9,5 +11,7 @@ module.exports = {
   SplitBox,
   // SplitBoxCSS,
   sprintf,
+  TreeRow,
+  TreeView,
   workerUtils,
 };
