4482a0bd30335b054a15eb06b6ba16628e216a8c
J. Ryan Stinnett <jryans@gmail.com>
2017-03-28 22:58:24 -0500

    Revert tab / tree references added by #282

--- packages/devtools-modules/index.js
+++ packages/devtools-modules/index.js
@@ -2,10 +2,6 @@ const Services = require("./client/shared/shim/Services");
 const SplitBox = require("./client/shared/components/splitter/SplitBox");
 // const SplitBoxCSS = require("./client/shared/components/splitter/SplitBox.css")
 const sprintf = require("./shared/sprintf").sprintf;
-const Tabbar = require("./client/shared/components/tabs/tabbar");
-const TabPanel = require("./client/shared/components/tabs/tabs").TabPanel;
-const TreeRow = require("./client/shared/components/tree/tree-row");
-const TreeView = require("./client/shared/components/tree/tree-view");
 const workerUtils = require("./shared/worker-utils");
 
 module.exports = {
@@ -13,9 +9,5 @@ module.exports = {
   SplitBox,
   // SplitBoxCSS,
   sprintf,
-  Tabbar,
-  TabPanel,
-  TreeRow,
-  TreeView,
   workerUtils,
 };
