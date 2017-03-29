64bdbdf020447bc088fe1ba9310d970e8bfaf1e6
Bryan Clark <clarkbw@gmail.com>
2017-03-29 15:22:31 -0500

    dumping path variables into the console

--- packages/devtools-launchpad/bin/validate-deps.js
+++ packages/devtools-launchpad/bin/validate-deps.js
@@ -1,10 +1,21 @@
 const fs   = require('fs');
 const path = require("path");
+const which = require('which');
 
 function isUnique(list) {
   const set = new Set(list)
   return set.size == list.length;
 }
+
+which('yarn', (err, whichPath) => {
+  if (err) throw err;
+  console.log('whichPath', whichPath);
+  fs.realpath(whichPath, (err, resolvedPath) => {
+    console.log('resolvedPath', resolvedPath);
+    const yarnPath = path.resolve(resolvedPath, '../../lib/lockfile/parse.js');
+    console.log('yarnPath', yarnPath);
+  });
+});
 
 const parser = require('parse-yarn-lock')
 const lockFilePath = path.normalize(path.join(__dirname, '..', 'yarn.lock'));
