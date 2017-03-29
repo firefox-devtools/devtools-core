78af276418fcdc28bda69bac00efd0a3b6fe26f7
Fred Lin <gasolin@mozilla.com>
2017-03-29 11:14:48 +0800

    Bundle missed devtools-modules for netmonitor

--- packages/devtools-modules/index.js
+++ packages/devtools-modules/index.js
@@ -1,15 +1,21 @@
+const clipboardHelper = require("./shared/clipboard");
+const { CurlUtils } = require("./client/shared/curl");
+const FileSaver = require("./client/shared/file-saver");
 const networkRequest = require("./client/shared/shim/networkRequest");
+const { PluralForm } = require("./shared/plural-form");
 const Services = require("./client/shared/shim/Services");
 const SplitBox = require("./client/shared/components/splitter/SplitBox");
-// const SplitBoxCSS = require("./client/shared/components/splitter/SplitBox.css")
 const sprintf = require("./shared/sprintf").sprintf;
 const workerUtils = require("./shared/worker-utils");
 
 module.exports = {
+  clipboardHelper,
+  CurlUtils,
+  FileSaver,
   networkRequest,
+  PluralForm,
   Services,
   SplitBox,
-  // SplitBoxCSS,
   sprintf,
   workerUtils,
 };
