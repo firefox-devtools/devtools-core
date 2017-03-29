a104e61eeb3cae1dda41cf15e0007d9fce20554c
Jason Laster <jason.laster.11@gmail.com>
2017-03-29 13:58:46 -0400

    Splitter - require css directly (#294)

--- packages/devtools-modules/index.js
+++ packages/devtools-modules/index.js
@@ -1,13 +1,11 @@
 const Services = require("./client/shared/shim/Services");
 const SplitBox = require("./client/shared/components/splitter/SplitBox");
-// const SplitBoxCSS = require("./client/shared/components/splitter/SplitBox.css")
 const sprintf = require("./shared/sprintf").sprintf;
 const workerUtils = require("./shared/worker-utils");
 
 module.exports = {
   Services,
   SplitBox,
-  // SplitBoxCSS,
   sprintf,
   workerUtils,
 };
