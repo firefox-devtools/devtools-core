23bb8b84ba7733fc6623d4123eb4976e5ec2edf0
Bryan Clark <clarkbw@gmail.com>
2017-03-29 15:29:15 -0500

    grrr

--- packages/devtools-launchpad/bin/validate-deps.js
+++ packages/devtools-launchpad/bin/validate-deps.js
@@ -14,25 +14,26 @@ which('yarn', (err, whichPath) => {
     console.log('resolvedPath', resolvedPath);
     const yarnPath = path.resolve(resolvedPath, '../../lib/lockfile/parse.js');
     console.log('yarnPath', yarnPath);
+
+    const parser = require('parse-yarn-lock')
+    const lockFilePath = path.normalize(path.join(__dirname, '..', 'yarn.lock'));
+    const lockfile = fs.readFileSync(lockFilePath).toString()
+
+    parser.parse(lockfile, function (err, deps) {
+      if (err) throw err
+
+      const depKeys = Object.keys(deps);
+      const devtoolDeps = depKeys.filter(dep => dep.match(/^devtools-/))
+
+      // check for duplicate dependencies
+      const depsList = devtoolDeps.map(dep => dep.replace(/@.*/,""))
+      const hasDuplicates = !isUnique(depsList);
+
+      if (hasDuplicates) {
+        console.warn(`Oops, it looks like there's duplicate dependencies:\n\n${devtoolDeps.join("\n")}\n\nCheck your yarn.lock file for the discrepancy.`)
+        process.exit(1);
+      }
+    });
+
   });
-});
-
-const parser = require('parse-yarn-lock')
-const lockFilePath = path.normalize(path.join(__dirname, '..', 'yarn.lock'));
-const lockfile = fs.readFileSync(lockFilePath).toString()
-
-parser.parse(lockfile, function (err, deps) {
-  if (err) throw err
-
-  const depKeys = Object.keys(deps);
-  const devtoolDeps = depKeys.filter(dep => dep.match(/^devtools-/))
-
-  // check for duplicate dependencies
-  const depsList = devtoolDeps.map(dep => dep.replace(/@.*/,""))
-  const hasDuplicates = !isUnique(depsList);
-
-  if (hasDuplicates) {
-    console.warn(`Oops, it looks like there's duplicate dependencies:\n\n${devtoolDeps.join("\n")}\n\nCheck your yarn.lock file for the discrepancy.`)
-    process.exit(1);
-  }
 });
