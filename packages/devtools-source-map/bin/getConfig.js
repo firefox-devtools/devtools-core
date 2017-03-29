3d33c72dd5a515190b97d156077b45b1e861f36a
J. Ryan Stinnett <jryans@gmail.com>
2017-03-28 19:49:31 -0500

    Set source map worker URL in Firefox

--- packages/devtools-source-map/bin/getConfig.js
+++ packages/devtools-source-map/bin/getConfig.js
@@ -4,10 +4,15 @@ const path = require("path");
 
 function getConfig() {
   const developmentConfig = require("../configs/development.json");
+  const firefoxConfig = require("../configs/firefox-panel.json");
 
   let localConfig = {};
   if (fs.existsSync(path.resolve(__dirname, "../configs/local.json"))) {
     localConfig = require("../configs/local.json");
+  }
+
+  if (process.env.TARGET === "firefox-panel") {
+    return firefoxConfig;
   }
 
   return merge({}, developmentConfig, localConfig);
