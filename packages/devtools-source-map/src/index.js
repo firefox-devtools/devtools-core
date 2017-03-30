8dd96c9deb46915f50933f5e40ad6a4cf7b2c6d7
Jason Laster <jason.laster.11@gmail.com>
2017-03-30 14:18:17 -0400

    (WIP) Improve Workers (#300)
    * Add devtools utils
    
    * Update source map worker

--- packages/devtools-source-map/src/index.js
+++ packages/devtools-source-map/src/index.js
@@ -1,5 +1,4 @@
 // @flow
-/* global DebuggerConfig */
 
 const {
   originalToGeneratedId,
@@ -8,57 +7,19 @@ const {
   isOriginalId,
 } = require("./util");
 
-const { workerUtils: { workerTask }} = require("devtools-modules");
-const { setConfig, getValue } = require("devtools-config");
+const { workerUtils: { WorkerDispatcher }} = require("devtools-utils");
 
 import type { Location } from "debugger-html";
 
-// TODO: Rename this to something not specific to debugger (#265)
-// $FlowIgnore: global DebuggerConfig
-setConfig(DebuggerConfig);
+const dispatcher = new WorkerDispatcher();
 
-let sourceMapWorker;
-function restartWorker() {
-  if (sourceMapWorker) {
-    sourceMapWorker.terminate();
-  }
-
-  sourceMapWorker = new Worker(getValue("workers.sourceMapURL"));
-  sourceMapWorker.onerror = () => {
-    console.error("Error in source map worker");
-  };
-}
-
-restartWorker();
-
-function destroyWorker() {
-  if (sourceMapWorker) {
-    sourceMapWorker.terminate();
-    sourceMapWorker = null;
-  }
-}
-
-async function hasMappedSource(location: Location): Promise<boolean> {
-  if (isOriginalId(location.sourceId)) {
-    return true;
-  }
-
-  const loc = await getOriginalLocation(location);
-  return loc.sourceId !== location.sourceId;
-}
-
-const getOriginalURLs = workerTask(sourceMapWorker, "getOriginalURLs");
-const getGeneratedLocation = workerTask(
-  sourceMapWorker,
-  "getGeneratedLocation",
-);
-const getOriginalLocation = workerTask(sourceMapWorker, "getOriginalLocation");
-const getOriginalSourceText = workerTask(
-  sourceMapWorker,
-  "getOriginalSourceText",
-);
-const applySourceMap = workerTask(sourceMapWorker, "applySourceMap");
-const clearSourceMaps = workerTask(sourceMapWorker, "clearSourceMaps");
+const getOriginalURLs = dispatcher.task("getOriginalURLs");
+const getGeneratedLocation = dispatcher.task("getGeneratedLocation");
+const getOriginalLocation = dispatcher.task("getOriginalLocation");
+const getOriginalSourceText = dispatcher.task("getOriginalSourceText");
+const applySourceMap = dispatcher.task("applySourceMap");
+const clearSourceMaps = dispatcher.task("clearSourceMaps");
+const hasMappedSource = dispatcher.task("hasMappedSource");
 
 module.exports = {
   originalToGeneratedId,
@@ -72,5 +33,6 @@ module.exports = {
   getOriginalSourceText,
   applySourceMap,
   clearSourceMaps,
-  destroyWorker,
+  start: dispatcher.start.bind(dispatcher),
+  stop: dispatcher.start.bind(dispatcher)
 };
---
+++ packages/devtools-utils/index.js
@@ -0,0 +1,7 @@
+const networkRequest = require("./src/network-request");
+const workerUtils = require("./src/worker-utils");
+
+module.exports = {
+  networkRequest,
+  workerUtils,
+};
