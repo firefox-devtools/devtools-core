64b05b8527aba1c21f7ef7fe248ea058171729b7
Jason Laster <jason.laster.11@gmail.com>
2017-04-08 11:57:11 -0400

    add vertical splitter story (#315)

--- packages/devtools-splitter/stories/index.js
+++ packages/devtools-splitter/stories/index.js
@@ -4,19 +4,37 @@ import SplitBox from "..";
 
 import "./index.css";
 
-storiesOf("Splitter", module).add("splitter", () => {
-  const start = <div style={{ padding: "10px" }}>Yo</div>;
-  const end = <div style={{ padding: "10px" }}>Bye</div>;
-  return (
-    <SplitBox
-      startPanel={start}
-      endPanel={end}
-      splitterSize={1}
-      initialSize="50%"
-      minSize={10}
-      vert={true}
-      endPanelControl={true}
-      onResizeEnd={action("resizeEnd")}
-    />
-  );
-});
+storiesOf("Splitter", module)
+  .add("splitter", () => {
+    const start = <div style={{ padding: "10px" }}>Yo</div>;
+    const end = <div style={{ padding: "10px" }}>Bye</div>;
+    return (
+      <SplitBox
+        startPanel={start}
+        endPanel={end}
+        splitterSize={1}
+        initialSize="50%"
+        minSize={10}
+        vert={true}
+        endPanelControl={true}
+        onResizeEnd={action("resizeEnd")}
+      />
+    );
+  })
+  .add("vertical mode", () => {
+    const start = <div style={{ padding: "10px" }}>Yo</div>;
+    const end = <div style={{ padding: "10px" }}>Bye</div>;
+    return (
+      <SplitBox
+        startPanel={start}
+        endPanel={end}
+        splitterSize={1}
+        initialSize="50%"
+        minSize={1}
+        vert={false}
+        endPanelControl={false}
+        onResizeEnd={action("resizeEnd")}
+        onMove={action("onMove")}
+      />
+    );
+  });
