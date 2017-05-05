f7d727a3ba91d786a4f8533b92ddd1604f320186
Patrick Brosset <pbrosset@mozilla.com>
2017-05-05 11:22:50 +0200

    Added MPL license to devtools-connection

--- packages/devtools-connection/src/utils/chrome.js
+++ packages/devtools-connection/src/utils/chrome.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /*
  * A sham for https://developer.mozilla.org/en-US/Add-ons/SDK/Low-Level_APIs/chrome
  */
