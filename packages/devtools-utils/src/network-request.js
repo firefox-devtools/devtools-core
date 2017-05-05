8b5524947c4c54b4a390344ac5e6bcc65b81e317
Patrick Brosset <pbrosset@mozilla.com>
2017-05-05 11:36:54 +0200

    Added MPL license to devtools-utils

--- packages/devtools-utils/src/network-request.js
+++ packages/devtools-utils/src/network-request.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // @flow
 
 // opts is ignored because this is only used in local development and
