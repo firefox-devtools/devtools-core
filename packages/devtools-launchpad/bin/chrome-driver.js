51dd742c7ad04120f957f719deabba4cc845c4e0
zaggy <ldsaint.sergey@gmail.com>
2017-05-30 00:13:40 +1100

    Fix git issues

--- packages/devtools-launchpad/bin/chrome-driver.js
+++ packages/devtools-launchpad/bin/chrome-driver.js
@@ -2,7 +2,7 @@
 
 const spawn = require('child_process').spawn;
 const minimist = require("minimist");
-<<<<<<< HEAD
+
 const os = require("os");
 
 const isWindows = /^win/.test(process.platform);
@@ -11,13 +11,6 @@ const chromeBinary = isWindows ?
   "c:\\program files (x86)\\google\\chrome\\application\\chrome.exe" :
   "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome";
 
-=======
-const os = require('os');
-
-const chromeBinary = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-const chromeBinaryWindows = 
-      "c:\\program files (x86)\\google\\chrome\\application\\chrome.exe"
->>>>>>> 665e4092ed16e57ab687a46ae9b2f8d7b71fdf0c
 const args = minimist(process.argv.slice(2), {
   string: ["location"]
 });
@@ -28,17 +21,9 @@ let chromeArgs = [
   `--user-data-dir=${os.tmpdir()}/chrome-dev-profile`
 ];
 
-let chromeArgsWindows = [
-  "--remote-debugging-port=9222",
-  "--no-first-run",
-  "--user-data-dir=" + os.tmpdir() + "\\chrome-dev-profile"
-];
-
-const isWindows = /^win/.test(process.platform);
-
 chromeArgs.push(args.location ? args.location : "about:blank");
 
-const chrome = isWindows ? spawn(chromeBinaryWindows, chromeArgsWindows) : spawn(chromeBinary, chromeArgs);
+const chrome = spawn(chromeBinary, chromeArgs);
 
 chrome.stdout.on('data', data => console.log(`stdout: ${data}`));
 chrome.stderr.on('data', data => console.log(`stderr: ${data}`));
