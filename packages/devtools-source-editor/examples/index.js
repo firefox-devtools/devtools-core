7b9ccf161272c10b128b154d812a2f54f1612cea
Patrick Brosset <pbrosset@mozilla.com>
2017-06-01 10:44:17 +0200

    Adding the missing LICENSE file and headers in source-editor

--- packages/devtools-source-editor/examples/index.js
+++ packages/devtools-source-editor/examples/index.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 const SourceEditor = require("../src/source-editor");
 
 require("devtools-launchpad/src/lib/themes/light-theme.css");
