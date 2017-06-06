8bb74272648d8a0dceb251f507b97e332c55fb07
Patrick Brosset <pbrosset@mozilla.com>
2017-06-06 12:10:10 +0200

    Adds the LICENSE file and missing headers

--- packages/devtools-launchpad/src/utils/redux/middleware/wait-service.js
+++ packages/devtools-launchpad/src/utils/redux/middleware/wait-service.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /**
  * A middleware which acts like a service, because it is stateful
  * and "long-running" in the background. It provides the ability
