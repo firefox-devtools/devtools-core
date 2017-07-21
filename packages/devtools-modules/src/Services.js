d3eb3d5319c74cc0f5e0da6d2b5b3b1357e7bce7
Julian Descottes <julian.descottes@gmail.com>
2017-07-21 23:02:56 +0200

    Support default values in preferences getters

--- packages/devtools-modules/src/Services.js
+++ packages/devtools-modules/src/Services.js
@@ -58,12 +58,19 @@ PrefBranch.prototype = {
   },
 
   /** @see nsIPrefBranch.getBoolPref.  */
-  getBoolPref: function(prefName) {
-    let thePref = this._findPref(prefName);
-    if (thePref._type !== PREF_BOOL) {
-      throw new Error(`${prefName} does not have bool type`);
+  getBoolPref: function(prefName, defaultValue) {
+    try {
+      let thePref = this._findPref(prefName);
+      if (thePref._type !== PREF_BOOL) {
+        throw new Error(`${prefName} does not have bool type`);
+      }
+      return thePref._get();
+    } catch (e) {
+      if (typeof defaultValue !== "undefined") {
+        return defaultValue;
+      }
+      throw e;
     }
-    return thePref._get();
   },
 
   /** @see nsIPrefBranch.setBoolPref.  */
@@ -79,12 +86,19 @@ PrefBranch.prototype = {
   },
 
   /** @see nsIPrefBranch.getCharPref.  */
-  getCharPref: function(prefName) {
-    let thePref = this._findPref(prefName);
-    if (thePref._type !== PREF_STRING) {
-      throw new Error(`${prefName} does not have string type`);
+  getCharPref: function(prefName, defaultValue) {
+    try {
+      let thePref = this._findPref(prefName);
+      if (thePref._type !== PREF_STRING) {
+        throw new Error(`${prefName} does not have string type`);
+      }
+      return thePref._get();
+    } catch (e) {
+      if (typeof defaultValue !== "undefined") {
+        return defaultValue;
+      }
+      throw e;
     }
-    return thePref._get();
   },
 
   /** @see nsIPrefBranch.setCharPref.  */
@@ -100,12 +114,19 @@ PrefBranch.prototype = {
   },
 
   /** @see nsIPrefBranch.getIntPref.  */
-  getIntPref: function(prefName) {
-    let thePref = this._findPref(prefName);
-    if (thePref._type !== PREF_INT) {
-      throw new Error(`${prefName} does not have int type`);
+  getIntPref: function(prefName, defaultValue) {
+    try {
+      let thePref = this._findPref(prefName);
+      if (thePref._type !== PREF_INT) {
+        throw new Error(`${prefName} does not have int type`);
+      }
+      return thePref._get();
+    } catch (e) {
+      if (typeof defaultValue !== "undefined") {
+        return defaultValue;
+      }
+      throw e;
     }
-    return thePref._get();
   },
 
   /** @see nsIPrefBranch.setIntPref.  */
