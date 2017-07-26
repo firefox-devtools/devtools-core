c4a1cc7193db188a946fb2ba899d50865cfc2f1e
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2017-07-26 08:39:50 +0200

    Fix getValue function for legitimate falsy values.
    The getValue was failing if the item has a falsy value (false, null, 0, …).
    This patch fixes this issue and adds some tests to ensure getValue works as expected.

--- packages/devtools-reps/src/object-inspector/tests/object-inspector.js
+++ packages/devtools-reps/src/object-inspector/tests/object-inspector.js
@@ -816,3 +816,89 @@ describe("getChildren", () => {
     expect(nodePaths).toEqual(childrenPaths);
   });
 });
+
+describe("getValue", () => {
+  it("get the value from contents.value", () => {
+    let item = {
+      contents: {
+        value: "my value"
+      }
+    };
+    expect(getValue(item)).toBe("my value");
+
+    item = {
+      contents: {
+        value: 0
+      }
+    };
+    expect(getValue(item)).toBe(0);
+
+    item = {
+      contents: {
+        value: false
+      }
+    };
+    expect(getValue(item)).toBe(false);
+
+    item = {
+      contents: {
+        value: null
+      }
+    };
+    expect(getValue(item)).toBe(null);
+  });
+
+  it("get the value from contents.getterValue", () => {
+    let item = {
+      contents: {
+        getterValue: "my getter value"
+      }
+    };
+    expect(getValue(item)).toBe("my getter value");
+
+    item = {
+      contents: {
+        getterValue: 0
+      }
+    };
+    expect(getValue(item)).toBe(0);
+
+    item = {
+      contents: {
+        getterValue: false
+      }
+    };
+    expect(getValue(item)).toBe(false);
+
+    item = {
+      contents: {
+        getterValue: null
+      }
+    };
+    expect(getValue(item)).toBe(null);
+  });
+
+  it("get the value from getter and setter", () => {
+    let item = {
+      contents: {
+        get: "get"
+      }
+    };
+    expect(getValue(item)).toEqual({get: "get"});
+
+    item = {
+      contents: {
+        set: "set"
+      }
+    };
+    expect(getValue(item)).toEqual({set: "set"});
+
+    item = {
+      contents: {
+        get: "get",
+        set: "set"
+      }
+    };
+    expect(getValue(item)).toEqual({get: "get", set: "set"});
+  });
+});
