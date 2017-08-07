2e4d296ca02f2efe35645d7094528c84c8a473c8
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2017-08-07 10:06:09 +0200

    Add a test for functions

---
+++ packages/devtools-reps/src/object-inspector/tests/component/function.js
@@ -0,0 +1,83 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { mount } = require("enzyme");
+const React = require("react");
+const { createFactory } = React;
+const ObjectInspector = createFactory(require("../../index"));
+const { MODE } = require("../../../reps/constants");
+
+const functionStubs = require("../../../reps/stubs/function");
+
+describe("ObjectInspector - functions", () => {
+  it("renders named function properties as expected", () => {
+    const stub = functionStubs.get("Named");
+    const oi = mount(ObjectInspector({
+      autoExpandDepth: 1,
+      roots: [{
+        path: "root",
+        name: "x",
+        contents: [{
+          path: "root/fn",
+          name: "fn",
+          contents: {value: stub}
+        }]
+      }],
+      getObjectProperties: () => {},
+      loadObjectProperties: () => {},
+      mode: MODE.LONG,
+    }));
+
+    const nodes = oi.find(".node");
+
+    const functionNode = nodes.last();
+    expect(functionNode.text()).toBe("testName()");
+  });
+
+  it("renders anon function properties as expected", () => {
+    const stub = functionStubs.get("Anon");
+    const oi = mount(ObjectInspector({
+      autoExpandDepth: 1,
+      roots: [{
+        path: "root",
+        name: "x",
+        contents: [{
+          path: "root/fn",
+          name: "fn",
+          contents: {value: stub}
+        }]
+      }],
+      getObjectProperties: () => {},
+      loadObjectProperties: () => {},
+      mode: MODE.LONG,
+    }));
+
+    const nodes = oi.find(".node");
+
+    const functionNode = nodes.last();
+    // It should have the name of the property.
+    expect(functionNode.text()).toBe("fn()");
+  });
+
+  it("renders top-level functions as expected", () => {
+    const stub = functionStubs.get("Named");
+    const oi = mount(ObjectInspector({
+      autoExpandDepth: 1,
+      roots: [{
+        path: "root",
+        name: "x",
+        contents: {value: stub}
+      }],
+      getObjectProperties: () => {},
+      loadObjectProperties: () => {},
+      mode: MODE.LONG,
+    }));
+
+    const nodes = oi.find(".node");
+
+    const functionNode = nodes.first();
+    // It should have the name of the property.
+    expect(functionNode.text()).toBe("function testName()");
+  });
+});
