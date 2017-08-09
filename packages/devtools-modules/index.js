b361afc818dfa40f80d8b14fd24772e51699ec64
Jan Odvarko <odvarko@gmail.com>
2017-08-09 15:36:01 +0200

    Unify the way how KeyShortcuts is exported with m-c

--- packages/devtools-modules/index.js
+++ packages/devtools-modules/index.js
@@ -6,7 +6,7 @@ const Menu = require("./src/menu");
 const MenuItem = require("./src/menu/menu-item");
 const { PrefsHelper } = require("./src/prefs");
 const Services = require("./src/Services");
-const { KeyShortcuts } = require("./src/key-shortcuts");
+const KeyShortcuts = require("./src/key-shortcuts");
 const { ZoomKeys } = require("./src/zoom-keys");
 const EventEmitter = require("./src/utils/event-emitter");
 
