297b66b35938359a05060aeed4d0f73208d3f9a7
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2017-08-29 17:59:18 +0200

    Fix eslint errors

--- packages/devtools-launchpad/src/components/Sidebar.js
+++ packages/devtools-launchpad/src/components/Sidebar.js
@@ -22,12 +22,16 @@ const Sidebar = React.createClass({
   },
 
   renderTitle(title) {
-
     return dom.div({className: "title-wrapper"},
-                    dom.h1({}, title),
-                    dom.div({className: "launchpad-container"},
-                            dom.div({className: "launchpad-container-icon", dangerouslySetInnerHTML: {__html: rocketSvg }}),
-                            dom.h2({className: "launchpad-container-title"}, "Launchpad")));
+      dom.h1({}, title),
+      dom.div({className: "launchpad-container"},
+        dom.div({
+          className: "launchpad-container-icon",
+          dangerouslySetInnerHTML: {__html: rocketSvg }
+        }),
+        dom.h2({className: "launchpad-container-title"}, "Launchpad")
+      )
+    );
   },
 
   renderItem(title) {
