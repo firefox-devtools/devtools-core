f1ba97f25cd3d3926039ada6e6e733f8ae2356ae
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2017-08-30 14:37:40 +0200

    Fix Tree component.
    Use an svg image for the arrow.
    Add proper styling.

--- packages/devtools-components/index.js
+++ packages/devtools-components/index.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const Tree = require("./src/tree");
+import Tree from "./src/tree";
 
-module.exports = {
+export default {
   Tree
 };
