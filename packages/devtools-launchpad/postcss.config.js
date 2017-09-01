06e17cd4ed0c17edc4fb047f733ef6f34525a50a
Jason Laster <jason.laster.11@gmail.com>
2017-09-01 17:31:54 -0400

    Remove theme namespaces

--- packages/devtools-launchpad/postcss.config.js
+++ packages/devtools-launchpad/postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require("postcss-bidirection"), require("autoprefixer")]
+  plugins: [
+    require("postcss-bidirection"),
+    require("autoprefixer"),
+    require("postcss-class-namespace")()
+  ]
 };
