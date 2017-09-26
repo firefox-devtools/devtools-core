db11bb847337630a9d9e712e558860f8b361a70f
Jason Laster <jason.laster.11@gmail.com>
2017-09-25 21:02:41 -0400

    Sync themes

--- packages/devtools-launchpad/postcss.config.js
+++ packages/devtools-launchpad/postcss.config.js
@@ -1,7 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var mapUrl = require("postcss-url-mapper");
+
+const urls = ["chrome://", "resource://"];
+
+const urlRegex = new RegExp(`(${urls.join("|")})`);
+
+function _mapUrl(url) {
+  const newUrl = url
+    .replace(urlRegex, "/assets/")
+    .replace(/devtools\/skin/, "devtools/client/themes")
+    .replace(/devtools\/content/, "devtools/client");
+  return newUrl;
+}
+
 module.exports = {
   plugins: [
     require("postcss-bidirection"),
     require("autoprefixer"),
-    require("postcss-class-namespace")()
+    require("postcss-class-namespace")(),
+    mapUrl(_mapUrl)
   ]
 };
