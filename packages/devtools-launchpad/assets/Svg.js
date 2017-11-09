22ffc364c3a697531d4bd21f395ef077727934c6
Jason Laster <jason.laster.11@gmail.com>
2017-11-09 11:27:25 -0500

    fix rocket (#791)

--- packages/devtools-launchpad/assets/Svg.js
+++ packages/devtools-launchpad/assets/Svg.js
@@ -1,5 +1,5 @@
 const React = require("react");
-import InlineSVG from "svg-inline-react";
+const { default: InlineSVG } = require("svg-inline-react");
 const { isDevelopment } = require("devtools-config");
 
 const svg = {
