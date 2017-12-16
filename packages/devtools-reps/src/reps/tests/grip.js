ca6f1b6b18b719abcdd0188b716254fe164ee83d
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2017-12-16 17:04:44 +0200

    Cleanup

--- packages/devtools-reps/src/reps/tests/grip.js
+++ packages/devtools-reps/src/reps/tests/grip.js
@@ -3,7 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /* global jest */
-import {getGripPreviewItems} from "../rep-utils"
 
 const { shallow } = require("enzyme");
 const {
