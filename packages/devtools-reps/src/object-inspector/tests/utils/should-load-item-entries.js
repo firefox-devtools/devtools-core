677bd7e970bcb47f983f8a0b762db6132f3a9bed
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2018-01-10 08:53:26 +0100

    Fix require paths in tests

--- packages/devtools-reps/src/object-inspector/tests/utils/should-load-item-entries.js
+++ packages/devtools-reps/src/object-inspector/tests/utils/should-load-item-entries.js
@@ -2,12 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const Utils = require("../../utils");
 const {
   createNode,
   getChildren,
   makeNodesForEntries,
+} = Utils.node;
+
+const {
   shouldLoadItemEntries,
-} = require("../../utils/node");
+} = Utils.loadProperties;
 
 const gripMapStubs = require("../../../reps/stubs/grip-map");
 const gripArrayStubs = require("../../../reps/stubs/grip-array");
