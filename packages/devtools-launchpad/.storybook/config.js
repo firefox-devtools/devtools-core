8ac9fe127abe0444584d7fe9f16f35e0d810775c
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2018-02-23 17:16:14 +0100

    Update devtools-launchpad to React 16

--- packages/devtools-launchpad/.storybook/config.js
+++ packages/devtools-launchpad/.storybook/config.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { configure } from '@kadira/storybook';
+import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../stories');
