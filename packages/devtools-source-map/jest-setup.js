1ed76bbd278ddb0ccc6b98ec3aeeed404e291a7a
Caleb St-Denis <caleb.stdenis@gmail.com>
2018-03-19 11:51:05 -0400

    Fix broken tests resulting from source-map upgrade (#5598)

--- packages/devtools-source-map/jest-setup.js
+++ packages/devtools-source-map/jest-setup.js
@@ -1,5 +1,6 @@
 // TODO: Remove this file after upgrading to Jest 22.4.1+
 // More info: https://github.com/facebook/jest/issues/687
+// and https://github.com/devtools-html/devtools-core/pull/995#discussion_r174317667
 console.debug = console.log
 
 
