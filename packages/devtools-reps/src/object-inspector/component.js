f8afde46b4015c5e76b29903a1db92d2dd477562
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2018-03-30 12:35:57 +0300

    Omit longString type from primitives

--- packages/devtools-reps/src/object-inspector/component.js
+++ packages/devtools-reps/src/object-inspector/component.js
@@ -168,7 +168,7 @@ class ObjectInspector extends Component {
   }
 
   setExpanded(item: Node, expand: boolean) {
-    if (nodeIsPrimitive(item) && !nodeIsLongString(item)) {
+    if (nodeIsPrimitive(item)) {
       return;
     }
 
