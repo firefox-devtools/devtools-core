4c81083759a184f1758bf7ac581dd706a8c76753
Julian Descottes <julian.descottes@gmail.com>
2019-05-14 19:16:59 +0200

    Require Menu and MenuItem with full path to override for mc bundle

--- packages/devtools-modules/index.js
+++ packages/devtools-modules/index.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const Menu = require("./src/menu");
-const MenuItem = require("./src/menu/menu-item");
 const { PrefsHelper } = require("./src/prefs");
 const KeyShortcuts = require("./src/key-shortcuts");
 const { ZoomKeys } = require("./src/zoom-keys");
@@ -16,8 +14,6 @@ const { getUnicodeHostname, getUnicodeUrlPath, getUnicodeUrl } =
 
 module.exports = {
   KeyShortcuts,
-  Menu,
-  MenuItem,
   PrefsHelper,
   ZoomKeys,
   asyncStorage,
