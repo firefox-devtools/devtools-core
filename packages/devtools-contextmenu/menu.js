b63607474ea8edbc3d3c2ebfdc78cff4354453db
Julian Descottes <julian.descottes@gmail.com>
2019-05-14 19:16:59 +0200

    Require Menu and MenuItem with full path to override for mc bundle

--- packages/devtools-contextmenu/menu.js
+++ packages/devtools-contextmenu/menu.js
@@ -2,7 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { Menu, MenuItem } = require("devtools-modules");
+const Menu = require("devtools-modules/src/menu");
+const MenuItem = require("devtools-modules/src/menu-item");
 
 function inToolbox() {
   try {
